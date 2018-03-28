75ab0d277700df1caacf3d85b9f68b404a460988
David Lo <david.lo@namely.com>
2018-03-28 19:38:15 -0400

    Default RequisiteStageRefIds to empty slice

--- pipeline/builder/builder_test.go
+++ pipeline/builder/builder_test.go
@@ -1,9 +1,12 @@
 package builder_test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/namely/k8s-pipeliner/pipeline/builder"
+	"github.com/namely/k8s-pipeliner/pipeline/builder/types"
 	"github.com/namely/k8s-pipeliner/pipeline/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -47,4 +50,33 @@ func TestBuilderAssignsPipelineConfiguration(t *testing.T) {
 	assert.True(t, spinnaker.KeepWaitingPipelines)
 	assert.True(t, spinnaker.LimitConcurrent)
 	assert.Equal(t, pipeline.Description, spinnaker.Description)
+}
+
+func TestBuilderAssignsRequisiteStageRefIds(t *testing.T) {
+	wd, _ := os.Getwd()
+	file := filepath.Join(wd, "testdata", "deployment.full.yml")
+
+	pipeline := &config.Pipeline{
+		DisableConcurrentExecutions: true,
+		KeepQueuedPipelines:         true,
+		Description:                 "fake description",
+		Stages: 										 []config.Stage{
+			{
+				Name: "Test Stage",
+				Deploy: &config.DeployStage{
+					Groups: []config.Group{
+						{
+							ManifestFile: file,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	builder := builder.New(pipeline)
+	spinnaker, err := builder.Pipeline()
+	require.NoError(t, err, "error generating pipeline json")
+
+	assert.Equal(t, []string{}, spinnaker.Stages[0].(*types.DeployStage).StageMetadata.RequisiteStageRefIds)
 }
